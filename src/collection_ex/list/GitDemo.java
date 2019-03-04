156239433c6093109568e57f9e7d75a1ae34c3b7
tulip丿郁 <19940577405@163.com>
2019-03-04 08:13:52 +0800

    测试 git 提交

---
+++ src/collection_ex/list/GitDemo.java
@@ -0,0 +1,15 @@
+package collection_ex.list;
+
+/**
+ * Description: <测试 git>
+ *
+ * @author tulip丿yu
+ * @create 2019/3/4
+ * @since 1.0.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("019/3/4\n" +
+                " * @since 1.0.0");
+    }
+}
